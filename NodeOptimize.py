0525bc9802e9cbab2936ad22e7d0e153cca8cb71
jisraeli <jisraeli@stanford.edu>
2014-07-21 13:28:49 -0700

    optimizes node, need to add predict method

--- NodeOptimize.py
+++ NodeOptimize.py
@@ -1,14 +1,18 @@
 import numpy
 import theano
+import sys
 import theano.tensor as T
+from theano import pp
+from sklearn import datasets
 
 
-def OptimalNode(x, y):
+
+def OptimalNode(x_train, y_train):
     rng = numpy.random
 
-    N = 400
-    feats = 784
-    D = (rng.randn(N, feats), rng.randint(size=N, low=0, high=2))
+    feats = len(x_train[0, :])
+    print "shape of x: ", numpy.shape(x_train)
+    D = (x_train, y_train)
     training_steps = 10000
 
     # Declare Theano symbolic variables
@@ -16,24 +20,29 @@ def OptimalNode(x, y):
     y = T.vector("y")
     w = theano.shared(rng.randn(feats), name="w")
     b = theano.shared(0., name="b")
-    chi = theano.shared(0., name="chi")
+    a = theano.shared(1., name="a")
     print "Initialize node as:"
-    print w.get_value(), b.get_value(), chi.get_value()
+    print w.get_value(), b.get_value(), a.get_value()
+    print "w is of length: ", len(w.get_value())
 
     # Construct Theano expression graph
-    p_1 = chi / (1 + T.exp(-T.dot(x, w) - b))   # Probability that target = 1
+    p_1 = a / (1 + T.exp(-T.dot(x, w) - b))  # Probability that target = 1
     prediction = p_1 > 0.5                    # The prediction thresholded
-    xent = -y * T.log(p_1) - (1-y) * T.log(1-p_1) # Cross-entropy loss function
-    cost = xent.mean() + 0.01 * (w ** 2).sum()# The cost to minimize
-    gw, gb = T.grad(cost, [w, b])             # Compute the gradient of the cost
+    xent = -y * T.log(p_1) - (1-y) * T.log(1-p_1)  # Cross-entropy loss
+    cost = xent.mean() + 0.01 * (w ** 2).sum()  # The cost to minimize
+    gw, gb, ga = T.grad(cost, [w, b, a])     # Compute the gradient of the cost
                                           # (we shall return to this in a
                                           # following section of this tutorial)
-
+    #print "w derivative: ", pp(gw), "\n"
+    #print "b derivative: ", pp(gb), "\n"
+    #print "a derivative: ", pp(ga)
+    #sys.exit()
     # Compile
-    train = theano.function(
-            inputs=[x,y],
-            outputs=[prediction, xent],
-            updates=((w, w - 0.1 * gw), (b, b - 0.1 * gb)))
+    train = theano.function(inputs=[x, y],
+                            outputs=[prediction, xent],
+                            updates=((w, w - 0.1 * gw), (b, b - 0.1 * gb),
+                                     (a, a - 0.1 * ga)))
+
     predict = theano.function(inputs=[x], outputs=prediction)
 
     # Train
@@ -41,8 +50,18 @@ def OptimalNode(x, y):
         pred, err = train(D[0], D[1])
 
     print "Optimized Node:"
-    print w.get_value(), b.get_value(), chi.get_value()
+    print w.get_value(), b.get_value(), a.get_value()
     print "target values for D:", D[1]
     print "prediction on D:", predict(D[0])
+    print "error: ", 1.0*sum(D[1]!=predict(D[0])) / len(D[1])
 
     return D
+
+# import some data to play with
+iris = datasets.load_iris()
+X = iris.data[:, :3]  # we only take the first two features.
+Y = iris.target
+inds = Y<2
+Y = Y[inds]
+X = X[inds, :]
+Node = OptimalNode(X, Y)
