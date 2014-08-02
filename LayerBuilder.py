fb9c3fdcf34cd7028e74d50d15e8ef994e803609
jisraeli <jisraeli@stanford.edu>
2014-08-01 18:54:39 -0700

    prototyping Gaussian nodes

--- LayerBuilder.py
+++ LayerBuilder.py
@@ -4,7 +4,7 @@ import sys
 import theano.tensor as T
 from theano import pp
 from sklearn import datasets
-from NodeOptimize import OptimalNode, EarlyStopNode
+from NodeOptimize import OptimalNode, EarlyStopNode, OptimalGaussian
 from sklearn.cross_validation import train_test_split
 from sklearn import preprocessing
 import math
@@ -29,6 +29,8 @@ def InitLayer(X_train_node, Y_train_node, X_validate_node, Y_validate_node,
     Layer = {}
     Node = OptimalNode(X_train_node, Y_train_node, bias=True, n_iter=n_iter,
                        alpha=alpha, minibatch=minibatch)
+    #Node = OptimalGaussian(X_train_node, Y_train_node, bias=False,
+    #                       n_iter=n_iter, alpha=alpha, minibatch=minibatch)
     Node = EarlyStopNode(Node, X_validate_node, Y_validate_node)
 
     Node['lr'] = epsilon
@@ -62,6 +64,8 @@ def NewNode(Layer, X_train_node, Y_train_node, X_validate_node,
 
     Node = OptimalNode(X_train_node, Y_pseudo, bias=True, n_iter=n_iter,
                        alpha=alpha, minibatch=minibatch)
+    #Node = OptimalGaussian(X_train_node, Y_pseudo, bias=False, n_iter=n_iter,
+    #                       alpha=alpha, minibatch=minibatch)
     Node = EarlyStopNode(Node, X_validate_node, y_pseudo_validate)
     Node['lr'] = epsilon
 
@@ -203,9 +207,8 @@ def PrintRates(Layer):
 
 
 def BuildLayer(NumNodes, X_train, Y_train, X_validate_layer, Y_validate_layer,
-               n_iter, alpha, epsilon=1.0, BoostDecay=False,
-               UltraBoosting=False, g_tol=0.01, g_final=0.0000001,
-               threshold=-0.01, minibatch=False, nodeCV_size=0.1):
+               n_iter, alpha, epsilon=1.0, Validation='Uniform',
+               minibatch=False, nodeCV_size=0.1):
     '''
     Builds a Layer by optimizing new nodes and adding them if they are useful.
     Here's how it works:
@@ -224,20 +227,34 @@ def BuildLayer(NumNodes, X_train, Y_train, X_validate_layer, Y_validate_layer,
                 stop building the layer
 
     '''
-    train_validate = train_test_split(X_train, Y_train, test_size=nodeCV_size)
-    [X_train_node, X_validate_node,
-        Y_train_node, Y_validate_node] = train_validate
+    print 'Building layer with ', Validation, 'Validation sets'
     print 'Initializing Layer..'
+    if Validation=='Uniform':
+        train_validate = train_test_split(X_train, Y_train,
+                                          test_size=nodeCV_size)
+        [X_train_node, X_validate_node,
+            Y_train_node, Y_validate_node] = train_validate
+    elif Validation=='Shuffled':
+        X_train_node= X_train
+        X_validate_node = X_validate_layer
+        Y_train_node = Y_train
+        Y_validate_node = Y_validate_layer
+    else:
+        print 'Undefined Validation option!'
+        print 'Supported options: Uniform, Shuffled'
+        print 'Exiting!'
+        sys.exit()
     Layer = InitLayer(X_train_node=X_train_node, Y_train_node=Y_train_node,
                       X_validate_node=X_validate_node,
                       Y_validate_node=Y_validate_node, n_iter=n_iter,
                       alpha=alpha, epsilon=epsilon, minibatch=minibatch)
     i = 0
     while i < NumNodes:
-        train_validate = train_test_split(X_train, Y_train,
-                                          test_size=nodeCV_size)
-        [X_train_node, X_validate_node,
-            Y_train_node, Y_validate_node] = train_validate
+        if Validation=='Shuffled':
+            train_validate = train_test_split(X_train, Y_train,
+                                              test_size=nodeCV_size)
+            [X_train_node, X_validate_node,
+                Y_train_node, Y_validate_node] = train_validate
         print 'Optimizing New Node...'
         Node = NewNode(Layer=Layer, X_train_node=X_train_node,
                        Y_train_node=Y_train_node,
@@ -311,7 +328,7 @@ def RunLayerBuilder(NumNodes, X, Y, n_iter, alpha, epsilon=0.01, test_size=0.3,
                     boostCV_size=0.2, nodeCV_size=0.1,
                     BoostDecay=False, UltraBoosting=False, g_final=0.0000001,
                     g_tol=0.01, threshold=-0.01, minibatch=False,
-                    SymmetricLabels=False):
+                    Validation='Shuffled', SymmetricLabels=False):
 
     print "creating training, validation, and testing sets..."
     train_test = train_test_split(X, Y, test_size=test_size)
@@ -338,10 +355,9 @@ def RunLayerBuilder(NumNodes, X, Y, n_iter, alpha, epsilon=0.01, test_size=0.3,
     Layer = BuildLayer(NumNodes=NumNodes-1, X_train=X_train, Y_train=Y_train,
                        X_validate_layer=X_validate_layer,
                        Y_validate_layer=Y_validate_layer,
-                       n_iter=n_iter, alpha=alpha, epsilon=1.0,
-                       BoostDecay=False, UltraBoosting=False, g_tol=0.01,
-                       g_final=0.0000001, threshold=-0.01, minibatch=False,
-                       nodeCV_size=0.1)
+                       n_iter=n_iter, alpha=alpha, epsilon=epsilon,
+                       minibatch=minibatch,
+                       Validation=Validation, nodeCV_size=0.1)
     stop = timeit.default_timer()
 
     print "Layer Building RunTime: ", stop - start
@@ -410,8 +426,9 @@ def RunLayerBuilder(NumNodes, X, Y, n_iter, alpha, epsilon=0.01, test_size=0.3,
     print "Scikit's linear SVR on original data, test error: ", err_SVM_lin
     print "Scikit's gaussian SVR on original data, test error: ", err_SVM_rbf
 
-    errs = [err_train, err_validate, err_test,
-            err_AB, err_LB, err_SVM_lin, err_SVM_rbf]
+    #errs = [err_train, err_validate, err_test,
+    #        err_AB, err_LB, err_SVM_lin, err_SVM_rbf]
+    errs = [err_train, err_validate, err_test]
     results = [X_test, Y_test, pred_test]
 
     return [errs, results, N]
